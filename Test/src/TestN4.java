e13aa88873338c85ade8ef5e8fce67cc25d95b5a
salee <saleem1984@gmail.com>
2018-09-17 21:08:50 +0100

    First Commit

---
+++ Test/src/TestN4.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.xml.soap.Text;
+
+
+public class TestN4 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			Runtime runtime = Runtime.getRuntime();
+		
+		Process process = runtime.exec("cmd /c start C:\\Users\\salee\\Desktop\\Onne_N4.jnlp");
+				
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	    
+		
+	}
+
+}
+
